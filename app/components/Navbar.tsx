aa3b2b62ba3419a413b4d03a03d42b6802f2a1ed
Maxwell Home <maxwellrichter1@gmail.com>
2023-07-30 02:10:29 -0700

    added navbar start, bold name

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import { useTheme } from 'next-themes'
+import React, {useState} from "react"
+
+interface NavItem {
+  label: string
+  page: string
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+  {
+    label:"Home",
+    page:"home",
+  },
+  {
+    label:"About",
+    page:"about",
+  },
+  {
+    label:"Projects",
+    page:"projects",
+  },
+]
+
 
 export const Navbar = () => {
+  const { systemTheme, theme, setTheme } = useTheme()
+  const curentTheme = theme === "system" ? systemTheme : theme
+  const [navbar, setNavbar] = useState(false) 
   return (
-    <div>Navbar</div>
+    <header className="w-full mx-auto px-4">
+      <div>
+        <div>
+          <div className='font-bold'>
+            <h2> Maxwell Richter </h2>
+
+          </div>
+        </div>
+        <div>
+          {NAV_ITEMS.map((item, idx) => {
+            return <a key={idx}>{item.label}</a>
+          })}
+        </div>
+      </div>
+    </header>
   )
 }
 export default Navbar
