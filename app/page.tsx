0310feaef2c1b25ddbbfa31366b7a0d033a9fac3
Maxwell Home <maxwellrichter1@gmail.com>
2023-08-04 13:16:34 -0700

    added mdx, formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-
+import HelloWorld from './title.mdx'
+import Subtitle from './subtitle.mdx'
 export default function page() {
   return (
-    <div className='h-screen flex md:justify-center justify-start bg-stone-500'>
-     <div className='flex md:w-5/12 items-start bg-blue-500 h-12'>
-        <div className="flex mx-5 text-xl" > Max's portfolio Site </div>
+    <div className='h-screen flex md:justify-center justify-start bg-stone-200'>
+     <div className='flex md:w-5/12 items-start h-12'>
+      <div className='mx-5'>
+        <div className='text-2xl'>
+          <HelloWorld/>
+        </div>
+          <Subtitle/>
+      </div>
       </div>
     </div>
   )
