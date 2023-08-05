e2936b906f9a186d4512ed2c48686d6be148119d
Maxwell Home <maxwellrichter1@gmail.com>
2023-08-05 16:34:01 -0700

    added to home and project pages
    created directories for projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
 import HelloWorld from './title.mdx'
 import Subtitle from './subtitle.mdx'
+import Github from './github.mdx'
+import HomeBody from './homeBody.mdx'
 export default function page() {
   return (
     <div className='h-screen flex md:justify-center justify-start bg-stone-200'>
      <div className='flex md:w-5/12 items-start h-12'>
       <div className='mx-5'>
-        <div className='text-2xl'>
+        <div className='text-4xl'>
           <HelloWorld/>
         </div>
+        <div className='text-lg flex justify-start'>
           <Subtitle/>
+        </div>
+        <div>
+          <HomeBody/>
+        </div>
+      </div>
+      <div>
       </div>
       </div>
     </div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,18 @@
+import Title from './Title.mdx'
+import Body from './Body.mdx'
 export default function page() {
     return (
-        <div className="bg-stone-200 w-screen h-screen">
-          <h1 className="text-gray-600 font-rubik h-full w-10/12 mx-auto text-2xl" > Max's projects Site  </h1> 
-        </div>
+          <div className='h-screen flex md:justify-center justify-start bg-stone-200'>
+            <div className='flex md:w-5/12 items-start h-12'>
+              <div className='mx-5'>
+                <div className='text-4xl'>
+                  <Title/>
+                </div>
+                <div>
+                  <Body/>
+                </div>
+              </div>
+            </div>
+          </div>
     )
   }
