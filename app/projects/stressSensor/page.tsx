ec49800675ca2a2650278d9c28b3d2569bcf3f3c
Max Richter <marichter@chapman.edu>
2024-08-25 16:43:07 -0700

    test commit

---
+++ app/projects/stressSensor/page.tsx
@@ -0,0 +1,18 @@
+import Title from './Title.mdx'
+import Body from './Body.mdx'
+export default function page() {
+    return (
+          <div className='h-screen flex md:justify-center justify-start bg-stone-200'>
+            <div className='flex lg:w-5/12 items-start h-12'>
+              <div className='mx-5'>
+                <div className='text-4xl'>
+                  <Title/>
+                </div>
+                <div>
+                  <Body/>
+                </div>
+              </div>
+            </div>
+          </div>
+    )
+  }
