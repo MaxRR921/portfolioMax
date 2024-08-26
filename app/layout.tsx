6ee012c9fd04da67f08b9c331d6ee72f272f3ed4
Max Richter <marichter@chapman.edu>
2024-08-25 18:52:37 -0700

    hello

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className="font-rubik">
+      <body className="font-rubik text-white">
         <ThemeProvider enableSystem={true} attribute="class">
           <Navbar/>
           {children}
