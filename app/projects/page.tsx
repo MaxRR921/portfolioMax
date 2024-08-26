18027ea2987f62ec9f1d896c940e5b77e2779e05
Max Richter <marichter@chapman.edu>
2024-08-25 22:36:50 -0700

    testing head text white

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,7 +6,7 @@ export default function page() {
           <div className='h-screen flex lg:justify-center justify-start bg-stone-200 text-black'>
             <div className='flex lg:w-5/12 items-start h-12'>
               <div className='mx-5'>
-                <div className='text-4xl'>
+                <div>
                   <Title/>
                 </div>
                 <div>
