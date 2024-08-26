749b104d2388ffae55963c9edb03d08490f523df
Max Richter <marichter@chapman.edu>
2024-08-25 23:30:23 -0700

    hello

--- app/projects/RopedTogether/page.tsx
+++ app/projects/RopedTogether/page.tsx
@@ -3,7 +3,7 @@ import Body from './Body.mdx'
 import Body2 from './Body2.mdx'
 export default function page() {
     return (
-          <div className='h-screen flex md:justify-center justify-start bg-stone-200'>
+          <div className='h-screen flex md:justify-center justify-start'>
             <div className='flex lg:w-5/12 items-start h-12'>
               <div className='mx-5'>
                 <div className='text-4xl'>
--- app/projects/stressSensor/page.tsx
+++ app/projects/stressSensor/page.tsx
@@ -2,7 +2,7 @@ import Title from './Title.mdx'
 import Body from './Body.mdx'
 export default function page() {
     return (
-          <div className='h-screen flex md:justify-center justify-start bg-stone-200'>
+          <div className='h-screen flex md:justify-center justify-start'>
             <div className='flex lg:w-5/12 items-start h-12'>
               <div className='mx-5'>
                 <div className='text-4xl'>
