108722a755c7689961fc3ed1b988df38ff2917ea
Max Richter <marichter@chapman.edu>
2024-08-26 00:37:52 -0700

    hello

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,27 +1,19 @@
-import React from "react";
-import Link from 'next/link';
-import { useRouter } from 'next/router';  // Import useRouter
+import { useTheme } from 'next-themes'
+import React, {useState} from "react"
+//import { Link } from "react-scroll/modules"
+import  Link  from 'next/link'
+import { RiMoonFill, RiSunLine } from "react-icons/ri"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
+
 
 export const Navbar = () => {
-  const router = useRouter();  // Use the useRouter hook to access the router object
-
   return (
     <div className='h-32 flex lg:justify-center justify-start bg-black'>
       <div className='flex lg:w-5/12 items-center'>
-        {/* Each link now wraps an <a> tag where the class is conditionally applied */}
-        <Link href="/">
-          <a className={`mx-5 rounded-md text-white ${router.pathname === "/" ? 'underline' : 'no-underline'}`}>
-            Home
-          </a>
-        </Link>
-        <Link href="/projects">
-          <a className={`rounded-md text-white ${router.pathname === "/projects" ? 'underline' : 'no-underline'}`}>
-            Projects
-          </a>
-        </Link>
+          <Link href = "/" className='mx-5 hover:bg-stone-700 rounded-md text-white custom-underline'> Home </Link>
+          <Link href = "/projects" className='hover:bg-stone-700 rounded-md text-white custom-underline'> Projects </Link>
       </div>
     </div>
-  );
+  )
 }
-
-export default Navbar;
+export default Navbar
