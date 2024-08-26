1eb6ebbd094fa3859fd35e055419963f2f1a0b6e
Max Richter <marichter@chapman.edu>
2024-08-25 23:51:14 -0700

    hello

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,13 @@ export default function page() {
           <div>marichter@chapman.edu | 503-509-2979</div>
         </div>
         <div className='font-bold flex justify-start'> 
-        <Link href="https://www.linkedin.com/in/maxwell-richter-0b8a671b9/" className='rounded-md underline pr-4'>
-          <span className='hover:bg-stone-400'>
+        <Link href="https://www.linkedin.com/in/maxwell-richter-0b8a671b9/" className='underline pr-4'>
+          <span className='hover:bg-stone-400 rounded-md'>
             LinkedIn&nbsp;
           </span>
         </Link>
-        <Link href="https://github.com/MaxRR921" className='rounded-md underline pl-4'>
-          <span className='hover:bg-stone-400'>
+        <Link href="https://github.com/MaxRR921" className='underline pl-4'>
+          <span className='hover:bg-stone-400 rounded-md'>
             GitHub
           </span>
         </Link>
