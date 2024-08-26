bdf32e376ea1c8c60322562be35afb1765419bba
Max Richter <marichter@chapman.edu>
2024-08-26 00:33:36 -0700

    hello

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,19 +1,26 @@
-import { useTheme } from 'next-themes'
-import React, {useState} from "react"
-//import { Link } from "react-scroll/modules"
-import  Link  from 'next/link'
-import { RiMoonFill, RiSunLine } from "react-icons/ri"
-import { IoMdMenu, IoMdClose } from "react-icons/io"
-
+import React from "react";
+import Link from 'next/link';
+import { useRouter } from 'next/router';  // Import useRouter
 
 export const Navbar = () => {
+  const router = useRouter();  // Use the useRouter hook to access router object
+
   return (
     <div className='h-32 flex lg:justify-center justify-start bg-black'>
       <div className='flex lg:w-5/12 items-center'>
-          <Link href = "/" className='mx-5 hover:bg-stone-700 rounded-md text-white custom-underline'> Home </Link>
-          <Link href = "/projects" className='hover:bg-stone-700 rounded-md text-white custom-underline'> Projects </Link>
+        <Link href="/">
+          <a className={`mx-5 rounded-md text-white custom-underline ${router.pathname === "/" ? 'underline' : ''}`}>
+            Home
+          </a>
+        </Link>
+        <Link href="/projects">
+          <a className={`rounded-md text-white custom-underline ${router.pathname === "/projects" ? 'underline' : ''}`}>
+            Projects
+          </a>
+        </Link>
       </div>
     </div>
-  )
+  );
 }
-export default Navbar
+
+export default Navbar;
