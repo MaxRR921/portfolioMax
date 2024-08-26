8cab3bf6180129e71a62b527ee356c77d8cd6893
Max Richter <marichter@chapman.edu>
2024-08-25 18:31:12 -0700

    added custom underline

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ export default function page() {
           <div>marichter@chapman.edu | 503-509-2979</div>
         </div>
         <div className='font-bold flex justify-start'> 
-          <Link href= "https://www.linkedin.com/in/maxwell-richter-0b8a671b9/" className='hover:bg-stone-300 rounded-md'> LinkedIn&nbsp;</Link>
-          <Link href = "https://github.com/MaxRR921" className='hover:bg-stone-300 rounded-md'>GitHub </Link>
+          <Link href= "https://www.linkedin.com/in/maxwell-richter-0b8a671b9/" className='hover:bg-black rounded-md'> LinkedIn&nbsp;</Link>
+          <Link href = "https://github.com/MaxRR921" className='hover:bg-black rounded-md'>GitHub </Link>
         </div>
         <div>
           <HomeBody/>
