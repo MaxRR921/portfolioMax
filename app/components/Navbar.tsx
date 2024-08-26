f20ab3199cfe9c3033c3f0307b3b44554177ea57
Max Richter <marichter@chapman.edu>
2024-08-26 00:28:36 -0700

    hello

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,8 +10,8 @@ export const Navbar = () => {
   return (
     <div className='h-32 flex lg:justify-center justify-start bg-black'>
       <div className='flex lg:w-5/12 items-center'>
-          <Link href = "/" className='mx-5 hover:bg-stone-300 rounded-md text-white custom-underline'> Home </Link>
-          <Link href = "/projects" className='hover:bg-stone-300 rounded-md text-white custom-underline'> Projects </Link>
+          <Link href = "/" className='mx-5 hover:bg-stone-700 rounded-md text-white custom-underline'> Home </Link>
+          <Link href = "/projects" className='hover:bg-stone-700 rounded-md text-white custom-underline'> Projects </Link>
       </div>
     </div>
   )
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,31 +10,31 @@ export default function page() {
                 <div>
                   <br>
                   </br>
-                  <Link href = '/projects/stressSensor' className='font-bold hover:bg-stone-300 rounded-md underline'>Fiber Optic Stress Seonsor<br></br></Link> 
+                  <Link href = '/projects/stressSensor' className='font-bold hover:bg-stone-700 rounded-md underline'>Fiber Optic Stress Seonsor<br></br></Link> 
                   Stess sensor to evaluate material characteristics of different tissues
                 </div>
                 <div>
                   <br>
                   </br>
-                  <Link href = '/projects/RopedTogether' className='font-bold hover:bg-stone-300 rounded-md underline'> Roped Together<br></br> </Link>
+                  <Link href = '/projects/RopedTogether' className='font-bold hover:bg-stone-700 rounded-md underline'> Roped Together<br></br> </Link>
                   3D mountain climbing game. Winner at Oregon Game Project Challenge 2022
                 </div>
                 <div>
                   <br>
                   </br>
-                  <Link href = '/projects/RopeSim' className='font-bold hover:bg-stone-300 rounded-md underline'> 3D Rope Simulation <br></br> </Link>
+                  <Link href = '/projects/RopeSim' className='font-bold hover:bg-stone-700 rounded-md underline'> 3D Rope Simulation <br></br> </Link>
                   Constraint based rope simulation created for Roped together using C# and the unity engine line renderer
                 </div>
                 <div>
                   <br>
                   </br>
-                  <Link href = '/projects/LayerJot' className='font-bold hover:bg-stone-300 rounded-md underline'> AI Hand Washing Tool <br></br> </Link>
-                  Continued work with company <Link href = 'https://www.layerjot.com/' className='font-bold hover:bg-stone-300 rounded-md underline'>LayerJot</Link>  on an AI computer vision hand washing coaching system
+                  <Link href = '/projects/LayerJot' className='font-bold hover:bg-stone-700 rounded-md underline'> AI Hand Washing Tool <br></br> </Link>
+                  Continued work with company <Link href = 'https://www.layerjot.com/' className='font-bold hover:bg-stone-700 rounded-md underline'>LayerJot</Link>  on an AI computer vision hand washing coaching system
                 </div>
                 <div>
                   <br>
                   </br>
-                  <Link href = '/projects/Dance' className='font-bold hover:bg-stone-300 rounded-md underline'> Dance Performance 3D procedural system <br></br> </Link>
+                  <Link href = '/projects/Dance' className='font-bold hover:bg-stone-700 rounded-md underline'> Dance Performance 3D procedural system <br></br> </Link>
                   Created software in Unity 3D for procedural camera movement, lighting and animation system for professional dance performance.
                 </div>
               </div>
