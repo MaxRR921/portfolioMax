72a03bc3b256c7949913aa0f1eba811ce87b1237
Max Richter <maxwellrichter1@gmail.com>
2024-12-26 18:04:17 -0800

    navbar changed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,46 +1,35 @@
-// "use client";
-// import { useTheme } from 'next-themes'
-// import React, {useState} from "react"
-// //import { Link } from "react-scroll/modules"
-// import  Link  from 'next/link'
-// import { RiMoonFill, RiSunLine } from "react-icons/ri"
-// import { IoMdMenu, IoMdClose } from "react-icons/io"
+'use client'
 
+import { useTheme } from 'next-themes'
+import React, { useState } from "react"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { RiMoonFill, RiSunLine } from "react-icons/ri"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
 
-// export const Navbar = () => {
-//   return (
-//     <div className='h-32 flex lg:justify-center justify-start bg-black'>
-//       <div className='flex lg:w-5/12 items-center'>
-//           <Link href = "/" className='mx-5 hover:bg-stone-700 rounded-md text-white custom-underline'> Home </Link>
-//           <Link href = "/projects" className='hover:bg-stone-700 rounded-md text-white custom-underline'> Projects </Link>
-//       </div>
-//     </div>
-//   )
-// }
-// export default Navbar
+export const Navbar = () => {
+  const pathname = usePathname()
 
-
-
-
-"use client";
-
-import Link from "next/link";
-import React from "react";
-
-const Navbar = () => {
   return (
-    <nav className="h-32 flex items-center bg-black">
-      <Link href="/" className="mx-5 hover:bg-stone-700 rounded-md text-white">
-        Home
-      </Link>
-      <Link
-        href="/projects"
-        className="hover:bg-stone-700 rounded-md text-white"
-      >
-        Projects
-      </Link>
-    </nav>
-  );
-};
+    <div className='h-32 flex lg:justify-center justify-start bg-black'>
+      <div className='flex lg:w-5/12 items-center'>
+        <Link 
+          href="/" 
+          className='mx-5 hover:bg-stone-700 rounded-md text-white custom-underline'
+          prefetch
+        >
+          Home
+        </Link>
+        <Link 
+          href="/projects"
+          className='hover:bg-stone-700 rounded-md text-white custom-underline'
+          prefetch
+        >
+          Projects
+        </Link>
+      </div>
+    </div>
+  )
+}
 
-export default Navbar;
+export default Navbar
