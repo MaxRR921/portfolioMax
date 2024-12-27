87b64968a291b0881abddf284e9f8f48043b5c7c
Max Richter <maxwellrichter1@gmail.com>
2024-12-26 17:44:51 -0800

    test

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,22 +1,19 @@
 import { useTheme } from 'next-themes'
 import React, {useState} from "react"
+//import { Link } from "react-scroll/modules"
 import  Link  from 'next/link'
 import { RiMoonFill, RiSunLine } from "react-icons/ri"
 import { IoMdMenu, IoMdClose } from "react-icons/io"
 
-const Navbar = () => {
+
+export const Navbar = () => {
   return (
     <div className='h-32 flex lg:justify-center justify-start bg-black'>
       <div className='flex lg:w-5/12 items-center'>
-        <Link href="/">
-          <a className="mx-5 hover:bg-stone-700 rounded-md text-white custom-underline">Home</a>
-        </Link>
-        <Link href="/projects">
-          <a className="mx-5 hover:bg-stone-700 rounded-md text-white custom-underline">Projects</a>
-        </Link>
+          <Link href = "/" className='mx-5 hover:bg-stone-700 rounded-md text-white custom-underline'> Home </Link>
+          <Link href = "/projects" className='hover:bg-stone-700 rounded-md text-white custom-underline'> Projects </Link>
       </div>
     </div>
-  );
-};
-
-export default Navbar;
+  )
+}
+export default Navbar
