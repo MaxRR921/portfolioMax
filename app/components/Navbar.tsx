8760ef606c71da3a45489368de91bcc322ee13b6
Max Richter <maxwellrichter1@gmail.com>
2024-12-26 17:59:26 -0800

    navbar changed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,46 @@
+// "use client";
+// import { useTheme } from 'next-themes'
+// import React, {useState} from "react"
+// //import { Link } from "react-scroll/modules"
+// import  Link  from 'next/link'
+// import { RiMoonFill, RiSunLine } from "react-icons/ri"
+// import { IoMdMenu, IoMdClose } from "react-icons/io"
+
+
+// export const Navbar = () => {
+//   return (
+//     <div className='h-32 flex lg:justify-center justify-start bg-black'>
+//       <div className='flex lg:w-5/12 items-center'>
+//           <Link href = "/" className='mx-5 hover:bg-stone-700 rounded-md text-white custom-underline'> Home </Link>
+//           <Link href = "/projects" className='hover:bg-stone-700 rounded-md text-white custom-underline'> Projects </Link>
+//       </div>
+//     </div>
+//   )
+// }
+// export default Navbar
+
+
+
+
 "use client";
-import { useTheme } from 'next-themes'
-import React, {useState} from "react"
-//import { Link } from "react-scroll/modules"
-import  Link  from 'next/link'
-import { RiMoonFill, RiSunLine } from "react-icons/ri"
-import { IoMdMenu, IoMdClose } from "react-icons/io"
 
+import Link from "next/link";
+import React from "react";
 
-export const Navbar = () => {
+const Navbar = () => {
   return (
-    <div className='h-32 flex lg:justify-center justify-start bg-black'>
-      <div className='flex lg:w-5/12 items-center'>
-          <Link href = "/" className='mx-5 hover:bg-stone-700 rounded-md text-white custom-underline'> Home </Link>
-          <Link href = "/projects" className='hover:bg-stone-700 rounded-md text-white custom-underline'> Projects </Link>
-      </div>
-    </div>
-  )
-}
-export default Navbar
+    <nav className="h-32 flex items-center bg-black">
+      <Link href="/" className="mx-5 hover:bg-stone-700 rounded-md text-white">
+        Home
+      </Link>
+      <Link
+        href="/projects"
+        className="hover:bg-stone-700 rounded-md text-white"
+      >
+        Projects
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
