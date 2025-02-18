89ba677be356fdb2f0577fee3997b0b1ad222102
Maxwell Richter <maxwellrichter1@gmail.com>
2025-02-17 20:21:07 -0800

    fix

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,34 +1,36 @@
-"use client"
+"use client";
 
-import React, { useState } from "react"
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import { RiMoonFill, RiSunLine } from "react-icons/ri"
-import { IoMdMenu, IoMdClose } from "react-icons/io"
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const Navbar = () => {
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   return (
-    <div className='h-32 flex lg:justify-center justify-start bg-stone-200 dark:bg-black'>
-      <div className='flex lg:w-5/12 items-center'>
-        <Link 
-          href="/" 
-          className="mx-5 hover:bg-stone-300 dark:hover:bg-stone-700 rounded-md text-black dark:text-white custom-underline"
-          prefetch
-        >
-          Home
+    <div className="h-32 flex lg:justify-center justify-start bg-stone-200 dark:bg-black">
+      <div className="flex lg:w-5/12 items-center">
+        <Link href="/" passHref>
+          <span
+            className={`mx-5 hover:bg-stone-300 dark:hover:bg-stone-700 rounded-md text-black dark:text-white custom-underline ${
+              pathname === "/" ? "font-bold" : ""
+            }`}
+          >
+            Home
+          </span>
         </Link>
-        <Link 
-          href="/projects"
-          className="hover:bg-stone-300 dark:hover:bg-stone-700 rounded-md text-black dark:text-white custom-underline"
-          prefetch
-        >
-          Projects
+        <Link href="/projects" passHref>
+          <span
+            className={`hover:bg-stone-300 dark:hover:bg-stone-700 rounded-md text-black dark:text-white custom-underline ${
+              pathname === "/projects" ? "font-bold" : ""
+            }`}
+          >
+            Projects
+          </span>
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
