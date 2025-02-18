3c9dc485ffb404528e9a06aeeeeae2fee7c37623
Maxwell Richter <maxwellrichter1@gmail.com>
2025-02-17 20:18:26 -0800

    fix

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,54 +1,34 @@
-"use client";
+"use client"
 
-import React, { useState, useEffect } from "react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { RiMoonFill, RiSunLine } from "react-icons/ri";
-import { useTheme } from "next-themes";
+import React, { useState } from "react"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { RiMoonFill, RiSunLine } from "react-icons/ri"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
 
 export const Navbar = () => {
-  const pathname = usePathname();
-  const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+  const pathname = usePathname()
 
   return (
-    <div className="h-32 flex lg:justify-center justify-start bg-white dark:bg-black shadow-md">
-      <div className="flex lg:w-5/12 items-center">
-        <Link href="/" passHref>
-          <span
-            className={`mx-5 cursor-pointer hover:bg-stone-300 dark:hover:bg-stone-700 rounded-md ${
-              pathname === "/" ? "font-bold" : ""
-            }`}
-          >
-            Home
-          </span>
+    <div className='h-32 flex lg:justify-center justify-start bg-stone-200 dark:bg-black'>
+      <div className='flex lg:w-5/12 items-center'>
+        <Link 
+          href="/" 
+          className="mx-5 hover:bg-stone-300 dark:hover:bg-stone-700 rounded-md text-black dark:text-white custom-underline"
+          prefetch
+        >
+          Home
         </Link>
-        <Link href="/projects" passHref>
-          <span
-            className={`cursor-pointer hover:bg-stone-300 dark:hover:bg-stone-700 rounded-md ${
-              pathname === "/projects" ? "font-bold" : ""
-            }`}
-          >
-            Projects
-          </span>
+        <Link 
+          href="/projects"
+          className="hover:bg-stone-300 dark:hover:bg-stone-700 rounded-md text-black dark:text-white custom-underline"
+          prefetch
+        >
+          Projects
         </Link>
       </div>
-
-      {/* Dark Mode Toggle */}
-      {mounted && (
-        <button
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-          className="ml-auto mr-5 p-2 bg-gray-200 dark:bg-gray-800 text-black dark:text-white rounded-md shadow-md"
-        >
-          {theme === "dark" ? <RiSunLine size={24} /> : <RiMoonFill size={24} />}
-        </button>
-      )}
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
