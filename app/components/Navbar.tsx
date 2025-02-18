3710ea79b7283b0daa09985c48beb4e34398ba2b
Maxwell Richter <maxwellrichter1@gmail.com>
2025-02-17 19:53:48 -0800

    fix

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,32 +1,25 @@
-"use client";
+"use client"
 
-import React, { useState, useEffect } from "react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { RiMoonFill, RiSunLine } from "react-icons/ri";
-import { IoMdMenu, IoMdClose } from "react-icons/io";
-import { useTheme } from "next-themes";
+import React, { useState } from "react"
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { RiMoonFill, RiSunLine } from "react-icons/ri"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
 
 export const Navbar = () => {
-  const pathname = usePathname();
-  const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+  const pathname = usePathname()
 
   return (
-    <div className="h-32 flex lg:justify-center justify-start bg-white dark:bg-black shadow-md">
-      <div className="flex lg:w-5/12 items-center">
-        <Link
-          href="/"
+    <div className='h-32 flex lg:justify-center justify-start bg-black'>
+      <div className='flex lg:w-5/12 items-center'>
+        <Link 
+          href="/" 
           className="mx-5 hover:bg-stone-300 dark:hover:bg-stone-700 rounded-md text-black dark:text-white custom-underline"
           prefetch
         >
           Home
         </Link>
-        <Link
+        <Link 
           href="/projects"
           className="hover:bg-stone-300 dark:hover:bg-stone-700 rounded-md text-black dark:text-white custom-underline"
           prefetch
@@ -34,9 +27,8 @@ export const Navbar = () => {
           Projects
         </Link>
       </div>
-
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
